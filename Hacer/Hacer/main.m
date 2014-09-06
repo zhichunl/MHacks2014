2cba3d089efc1a287c7cffecd05c578da390a765
zhichunl <liyaya95@gmail.com>
2014-09-06 00:37:40 -0400

    initial commit

---
+++ Hacer/Hacer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hacer
+//
+//  Created by Zhichun Li on 9/6/14.
+//  Copyright (c) 2014 XSZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HCAppDelegate class]));
+    }
+}
---
+++ Hacer/HacerTests/HacerTests.m
@@ -0,0 +1,34 @@
+//
+//  HacerTests.m
+//  HacerTests
+//
+//  Created by Zhichun Li on 9/6/14.
+//  Copyright (c) 2014 XSZ. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HacerTests : XCTestCase
+
+@end
+
+@implementation HacerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
